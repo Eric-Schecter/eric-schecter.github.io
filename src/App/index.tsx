e69fd3b793863021c21f838586d244fb7c6da5c8
Eric-Schecter <eric199002@icloud.com>
2023-01-19 11:00:30 +0800

    init

---
+++ src/App/index.tsx
@@ -0,0 +1,10 @@
+import React  from 'react';
+import styles from './index.module.scss';
+
+export const App = () => {
+  return <div
+    className={styles.main}
+  >
+    This is my homepage
+  </div>
+}
