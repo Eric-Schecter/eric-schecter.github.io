ebdf256c9ddf9ad1a8712a13a4135b1677606b89
Eric <eric199002@icloud.com>
2026-08-05 22:54:11 +0800

    fix bug

--- static/js/preload.js
+++ static/js/preload.js
@@ -1,5 +1,5 @@
 // 超短版：10 行核心逻辑
-(function() {
+(function () {
   const resources = [
     '/assets/audios/bgm.mp3',
   ];
@@ -7,15 +7,18 @@
   function loadAll(callback) {
     const promises = resources.map(url => {
       return new Promise((resolve, reject) => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = url;
-        link.onload = resolve;
-        link.onerror = reject;
-        document.head.appendChild(link);
+        fetch(url)
+          .then(response => {
+            if (response.ok) {
+              resolve();
+            } else {
+              reject(new Error(`HTTP ${response.status}`));
+            }
+          })
+          .catch(reject);
       });
     });
-    
+
     Promise.allSettled(promises).then(() => {
       if (typeof callback === 'function') callback();
     });
